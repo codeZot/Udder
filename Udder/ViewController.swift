1cb775f86b0784876f8849d96fef8dad9d03b672
Jelle Dullaert <jelledullaert1988@gmail.com>
2024-03-27 16:03:38 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jelle Dullaert on 27/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
